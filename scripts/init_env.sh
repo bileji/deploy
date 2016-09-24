8ecdd6a0a13a18a3dfa3f89ef09ef69ebbe118cf
shuchao <645904877@qq.com>
2016-09-25 00:59:36 +0800

    fix bug

--- scripts/init_env.sh
+++ scripts/init_env.sh
@@ -5,11 +5,11 @@ LOG_DIRS=("mysql" "redis" "mongodb" "nginx" "php")
 for DIR in ${DB_DIRS[*]}
 do
 	DB_PATH_NAME="${MOUNT/%\//}/db/${DIR}"
-	rm -rf ${DB_PATH_NAME} && mkdir -p ${DB_PATH_NAME} && chmod 777 -R ${DB_PATH_NAME}
+	rm -rf ${DB_PATH_NAME} && mkdir -p ${DB_PATH_NAME} && chmod -R 777 ${DB_PATH_NAME}
 done
 
 for DIR in ${LOG_DIRS[*]}
 do
 	LOG_PATH_NAME="${MOUNT/%\//}/logs/${DIR}"
-	rm -rf ${LOG_PATH_NAME} && mkdir -p ${LOG_PATH_NAME} && chmod 777 -R ${LOG_PATH_NAME}
+	rm -rf ${LOG_PATH_NAME} && mkdir -p ${LOG_PATH_NAME} && chmod -R 777 ${LOG_PATH_NAME}
 done
