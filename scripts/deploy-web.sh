5ab2c055a55c191deafd1bfcd690d07587f507b9
shuchao <645904877@qq.com>
2016-10-10 14:44:56 +0800

    more

--- scripts/deploy-web.sh
+++ scripts/deploy-web.sh
@@ -13,11 +13,14 @@ done
 echo "\033[32minit dir done!\033[0m";
 
 # 拷贝密钥
-cp -r /data/deploy/volumes/ssh/* ~/.ssh > /dev/null 2>&1 || { echo >&2 "\033[31msh key copy failed.\033[0m"; exit 1; }
+cp -r /data/deploy/volumes/ssh/* ~/.ssh > /dev/null 2>&1 && chmod 600 ~/.ssh/docker || { echo >&2 "\033[31msh key copy failed.\033[0m"; exit 1; }
 
 # 克隆项目代码
 command -v git > /dev/null 2>&1 || { echo >&2 "\033[31mi need git but it's not installed. aborting.\033[0m"; exit 1; }
 mkdir -p /data/www/htdocs && cd /data/www/htdocs
-git clone -b cola dreamix-git:cola && git clone -b HuaXi dreamix-git:tsb-server
+git clone -b new dreamix-git:tsb-chat
 git clone -b php7 dreamix-git:tsb-passport
-git clone -b HuaXi-release dreamix-git:tsb-web
+git clone -b HX-release dreamix-git:tsb-web
+git clone -b cola dreamix-git:cola && git clone -b HuaXi dreamix-git:tsb-server
+
+# 修改配置
