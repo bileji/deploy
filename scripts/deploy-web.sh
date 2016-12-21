678761c794aa8730c7221d4866e680ebaa6083aa
shuchao <645904877@qq.com>
2016-12-21 11:49:31 +0800

    fix bug

--- scripts/deploy-web.sh
+++ scripts/deploy-web.sh
@@ -55,11 +55,13 @@ echo -e '\033[32mClone project done!\033[0m';
 # 获取mongodb and mysql username password
 MYSQL_USERNAME=`cat ${DEPLOY_ROOT/%\//}/docker-db.yml|grep MYSQL_USER|awk -F ":" '{print $2}'|tr -d '[ ]'`
 MYSQL_PASSWORD=`cat ${DEPLOY_ROOT/%\//}/docker-db.yml|grep MYSQL_PASSWORD|awk -F ":" '{print $2}'|tr -d '[ ]'`
+MONGO_DATABASE=`cat ${DEPLOY_ROOT/%\//}/docker-db.yml|grep MONGODB_DATABASE|awk -F ":" '{print $2}'|tr -d '[ ]'`
 MONGO_USERNAME=`cat ${DEPLOY_ROOT/%\//}/docker-db.yml|grep MONGODB_USER|awk -F ":" '{print $2}'|tr -d '[ ]'`
 MONGO_PASSWORD=`cat ${DEPLOY_ROOT/%\//}/docker-db.yml|grep MONGODB_PASS|awk -F ":" '{print $2}'|tr -d '[ ]'`
 
 MYSQL_USERNAME=${MYSQL_USERNAME//\"}
 MYSQL_PASSWORD=${MYSQL_PASSWORD//\"}
+MONGO_DATABASE=${MONGO_DATABASE//\"}
 MONGO_USERNAME=${MONGO_USERNAME//\"}
 MONGO_PASSWORD=${MONGO_PASSWORD//\"}
 
@@ -80,7 +82,7 @@ sed -Ei "s/'login'.+?/'login' => '${MYSQL_USERNAME}',/g" /data/www/htdocs/tsb-se
 sed -Ei "s/'password'.+?/'password' => '${MYSQL_PASSWORD}',/g" /data/www/htdocs/tsb-server/app/config/database.php
 sed -Ei "/mongo/{n;n;n;s/'login'.+?/'login' => '${MONGO_USERNAME}',/}" /data/www/htdocs/tsb-server/app/config/database.php
 sed -Ei "/mongo/{n;n;n;n;s/'password'.+?/'password' => '${MONGO_PASSWORD}',/}" /data/www/htdocs/tsb-server/app/config/database.php
-sed -Ei "/mongo/{n;n;n;n;n;s/'database'.+?/'database' => 'admin',/}" /data/www/htdocs/tsb-server/app/config/database.php
+sed -Ei "/mongo/{n;n;n;n;n;s/'database'.+?/'database' => '${MONGO_DATABASE}',/}" /data/www/htdocs/tsb-server/app/config/database.php
 
 echo -e '\033[32mModified server config done!\033[0m';
 
@@ -88,7 +90,7 @@ echo -e '\033[32mModified server config done!\033[0m';
     echo "DB_HOST='db.pro.com'"; \
     echo "DB_USERNAME='${MONGO_USERNAME}'"; \
     echo "DB_PASSWORD='${MONGO_PASSWORD}'"; \
-    echo "DB_DATABASE='admin'"; \
+    echo "DB_DATABASE='${MONGO_DATABASE}'"; \
     echo "REDIS_HOST='db.pro.com'"; \
 } > /data/www/htdocs/tsb-passport/.env && { echo -e >&2 '\033[32mModified passport config done!\033[0m'; }
 
